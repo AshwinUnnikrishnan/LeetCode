baf878e7b7ec139225639ea9c3403d86c338126c
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2023-10-23 15:38:50 -0400

    Time: 36 ms (83.47%), Space: 16.2 MB (48.40%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        buc = [0] *3
+        for i in nums:
+            buc[i] += 1
+        print(buc)
+        j = 0
+        for i in range(len(nums)):
+            while buc[j] == 0:
+                j += 1
+            nums[i] = j
+            buc[j] -= 1
+        return nums
