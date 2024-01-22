91c9e67324ef98774d0f200e3a3d7a55ab7043c7
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-01-21 19:48:00 -0500

    Time: 128 ms (79.51%), Space: 18 MB (60.25%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,8 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero = 0  # position of the first zero
-        for i in range(len(nums)):
+        i = 0
+        n = len(nums)
+        j = 0
+        def swap(i,j):
+            t = nums[i]
+            nums[i] = nums[j]
+            nums[j] = t
+    
+        while i<n:
+            
             if nums[i] != 0:
-                nums[i], nums[zero] = nums[zero], nums[i]
-                zero += 1
+                swap(i,j)
+                j += 1
+            i += 1
