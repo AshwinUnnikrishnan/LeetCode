58568467f93b2bb1beb4dca609cc251fa64c01c7
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-01-29 00:36:08 -0500

    Time: 598 ms (87.99%), Space: 20.9 MB (57.68%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,38 @@
 class Solution:
+
+            
+            
+    
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+            
+        def twoSum(nums, target):
+            left, right = 0, len(nums) - 1
+            result = []
+            while left < right:
+
+                tot = nums[left] + nums[right]
+
+                if tot == target:
+                    result.append([0-target, nums[left], nums[right]])
+                    left += 1
+                    while left<right and nums[left-1] == nums[left]:
+                        left += 1
+                elif tot < target:
+                    left += 1
+                else:
+                    right -= 1
+            return result
+                
         temp = nums.sort()
         result = []
-        n = len(nums)
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
-                continue
-            l, r = i + 1, n - 1
-            
-            while l<r:
-                
-                curSum = nums[i] + nums[l] + nums[r]
-                if curSum > 0:
-                    r -= 1
-                elif curSum<0:
-                    l += 1
-                else:
-                    result.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l-1] and l < r:
-                        l += 1
-                
+        i = 0
+        while i < len(nums):
+            res = twoSum( nums[i+1:] ,0 - nums[i])
+            if res:
+                result += res
+            i += 1
+            while i< len(nums) and nums[i-1] == nums[i]:
+                i += 1
+
         
         return result
