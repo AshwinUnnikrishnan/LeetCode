1a882171d1bf7d758b6fb80e41e4d873e0fccda4
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-02-01 12:43:31 -0500

    Time: 165 ms (52.24%), Space: 18.7 MB (55.67%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        
+        st = []
+        n = len(nums) - 1
+        res = [-1] * (n + 1)
+        
+        while n>=0:
+            
+            if len(st) == 0:
+                st.append(nums[n])
+                n -= 1
+            
+            elif st[-1] > nums[n]:
+                res[n] = st[-1]
+                st.append(nums[n])
+                n -= 1
+            
+            elif st[-1] <= nums[n]:
+                while len(st) != 0 and st[-1] <= nums[n]:
+                    st.pop()
+        n = len(nums)-1
+        print(st)
+        print("ASD")
+        while len(st) != 0:
+            while len(st) != 0 and st[-1] <= nums[n]:
+                    st.pop()
+            if len(st) == 0:
+                break
+            print(st)
+
+            res[n] = st[-1]
+            st.append(nums[n])
+            n -= 1
+        return res
+                
+        
