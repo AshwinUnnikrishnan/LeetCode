b7f8780570b3333dccd38653d2d1bd5eea4acbe5
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-02-03 17:22:17 -0500

    Time: 131 ms (9.26%), Space: 16.8 MB (70.39%) - LeetHub

---
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -0,0 +1,41 @@
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        
+        def checkStart(val):
+            temp = val.split(":")
+            if temp[1] == "start":
+                return True, int(temp[0]), int(temp[2])
+            return False, int(temp[0]), int(temp[2])
+            
+            
+        
+        func = [0]*n
+        i, n = 0, len(logs)
+        stack = []
+        
+        while i<n:
+            start, functionID, timeStamp = checkStart(logs[i])
+            
+            if len(stack) == 0:
+                stack.append([functionID, timeStamp])
+                i += 1
+            else:
+                
+                if start:
+                    currentStackTopFunctionId = stack[-1][0]
+                    func[currentStackTopFunctionId] += timeStamp - stack[-1][1]
+                    stack.append([functionID, timeStamp])
+                    i += 1
+                else:
+                    currentStackTopFunctionId = stack[-1][0]
+
+                    func[currentStackTopFunctionId] += timeStamp - stack[-1][1] + 1
+                    stack.pop()
+                    if len(stack)!= 0:
+                        stack[-1][1] = timeStamp+1
+
+                    
+                    i += 1
+            print(func)
+        return func
+        
