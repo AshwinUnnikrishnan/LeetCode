2d0cb788c3e1cd213c54e89ff05de0cc319a21e7
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-02-06 22:14:36 -0500

    Time: 30 ms (95.03%), Space: 16.8 MB (66.98%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        
+        def getCombinations(oBCount, cBCount, current, result, n):
+            
+            if oBCount == n and cBCount == n:
+                print(current)
+                result.append(current)
+            
+            if oBCount < n:
+                getCombinations(oBCount+1, cBCount, current+"(", result, n)
+            
+            if cBCount < oBCount and cBCount < n:
+                getCombinations(oBCount, cBCount+1, current+")", result, n)
+            
+        result = []
+        getCombinations(0,0,"", result,n)
+        return result
+
+        
