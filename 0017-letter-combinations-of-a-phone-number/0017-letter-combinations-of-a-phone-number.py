439c6582caaa2445395afe4c766322568f14ff9b
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-02-07 21:21:42 -0500

    Time: 34 ms (75.86%), Space: 16.4 MB (82.88%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -5,13 +5,14 @@ class Solution:
         mainDict = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         
         
-        def dfs(result, curr, digit, i, n):
-            if i == n:
+        def dfs(result, curr, digit):
+            if digit == "":
                 result.append(curr)
                 return
             strV = mainDict[digit[0]]
             for k in range(len(strV)):
-                dfs(result, curr+strV[k], digit[1:],i+1, n)
+                dfs(result, curr+strV[k], digit[1:])
+        
         result = []
-        dfs(result, "", digits, 0, len(digits))
+        dfs(result, "", digits)
         return result
