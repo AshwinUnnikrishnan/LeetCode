639b111044c41b23a7cb61dfa04225af78cfee1b
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-02-09 12:16:43 -0500

    Time: 605 ms (14.94%), Space: 32.2 MB (87.06%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,18 +2,28 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         
         def is_palindrome(substring):
+            print("-----------------")
+            print(substring)
+            print(substring == substring[::-1])
+            print("-----------------")
+
             return substring == substring[::-1]
 
-        def backtrack(start, path):
-            if start == len(s):
-                result.append(path[:])
+        def dfs(result, current, remaining):
+            
+            if len(remaining) == 0:
+                print(current)
+                result.append(current.copy())
                 return
+            for i in range(1,len(remaining)+1):
+                val = remaining[:i]
+                #print(val)
 
-            for end in range(start + 1, len(s) + 1):
-                current_substring = s[start:end]
-                if is_palindrome(current_substring):
-                    backtrack(end, path + [current_substring])
-
+                if is_palindrome(val):
+                    current.append(val)
+                    dfs(result, current, remaining[i:])
+                    current.pop()
         result = []
-        backtrack(0, [])
+        dfs(result, [], s)
         return result
+                
