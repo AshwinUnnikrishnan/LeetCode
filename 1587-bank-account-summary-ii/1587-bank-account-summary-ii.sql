efed53f8046a6c1a17a0f0ab64b7736cd127fd80
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-02-19 22:27:43 -0500

    Time: 1194 ms (99.73%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,6 +1,9 @@
+# Write your MySQL query statement below
+
 SELECT u.name as NAME, SUM(t.amount) as BALANCE
 FROM Users u
 LEFT JOIN Transactions t
 ON u.account = t.account
 GROUP BY u.name
 HAVING SUM(t.amount) > 10000;
+
