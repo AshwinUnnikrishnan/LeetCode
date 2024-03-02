8de86088cac25119f3a9864232fbbc355ced8b49
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-03-01 22:12:32 -0500

    Time: 1081 ms (15.99%), Space: 19.2 MB (99.75%) - LeetHub

---
+++ 0817-linked-list-components/0817-linked-list-components.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        
+        temp = head
+        count = 0
+        len_list = 0
+        while temp:
+            temp = temp.next
+            len_list +=1
+        
+        temp = head
+        
+        
+        dictV = [True if v in nums else False for v in range(len_list) ]
+
+        while(temp):
+            flag = False
+            while temp and dictV[temp.val]:
+                nums.remove(temp.val)
+                temp = temp.next
+                flag = True
+            if temp and not dictV[temp.val]:
+                temp = temp.next
+            if flag:
+                count += 1
+        return count
+                
+                
