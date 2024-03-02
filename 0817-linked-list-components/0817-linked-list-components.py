7c965b576fd7096f1d7ff4aedc10630194d614b9
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-03-01 22:13:54 -0500

    Time: 80 ms (59.14%), Space: 20.6 MB (77.16%) - LeetHub

--- 0817-linked-list-components/0817-linked-list-components.py
+++ 0817-linked-list-components/0817-linked-list-components.py
@@ -1,33 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        nums_set = set(nums)  # Convert nums list to a set for faster lookup
         
-        temp = head
         count = 0
-        #len_list = 0
-        #while temp:
-        #    temp = temp.next
-        #    len_list +=1
-        
-        #temp = head
-        
-        
-        #dictV = [True if v in nums else False for v in range(len_list) ]
 
-        while(temp):
-            flag = False
-            while temp and temp.val in nums:
-                nums.remove(temp.val)
-                temp = temp.next
-                flag = True
-            if temp and temp.val not in nums:
-                temp = temp.next
-            if flag:
+        temp = head
+        while temp:
+            if temp.val in nums_set:  # Check if the value is present in nums_set
                 count += 1
+                while temp and temp.val in nums_set:
+                    nums_set.remove(temp.val)
+                    temp = temp.next
+            else:
+                temp = temp.next
+        
         return count
-                
-                
