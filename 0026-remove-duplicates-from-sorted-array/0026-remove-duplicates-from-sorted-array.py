666970c605e58d77d82f2246ca5598bb4fdc8090
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-04-13 14:48:33 -0400

    Time: 72 ms (77.07%), Space: 17.9 MB (58.39%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -6,6 +6,7 @@ class Solution:
         k = 1  # Initialize k to count unique elements
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1]:
-                nums[k] = nums[i]  # Overwrite nums[k] with unique element
+                if i != k:
+                    nums[k] = nums[i]  # Overwrite nums[k] with unique element
                 k += 1  # Increment k
         return k
