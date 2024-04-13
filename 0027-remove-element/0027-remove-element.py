c7c4487c2fdea67ae66dc5016da048188619f68d
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-04-13 14:44:44 -0400

    Time: 35 ms (73.72%), Space: 16.5 MB (48.94%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         k = 0  # Initialize k to count non-val elements
-        for i in range(len(nums)):
+        for i in range(v:=len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]  # Overwrite nums[k] with non-val element
                 k += 1  # Increment k
