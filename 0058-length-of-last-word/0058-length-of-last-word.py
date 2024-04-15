350988c371016affeecb6d342bf35ea1042a7442
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-04-15 11:18:52 -0400

    Time: 33 ms (69.45%), Space: 16.5 MB (98.66%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -4,11 +4,6 @@ class Solution:
         
         def filt(word):
             return word if word!='' else None
-        words = filter(filt, words)
-        words = list(words)
-        print(words)
-        print(len(words))
-
-        print(type(words))
-        print(words[-1])
+        words = list(filter(filt, words))
+        
         return len((words)[-1])
