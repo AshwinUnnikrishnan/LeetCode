6c33130ef7355ac5d970cc9c35dd42112409f04d
ASHWIN UNNIKRISHNAN <unnikrishnan.a@northeastern.edu>
2024-04-19 19:24:13 -0400

    Time: 3 ms (98.65%), Space: 13.7 MB (40.40%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numIndexMap;
+        map<int, int> numIndexMap;
 
         // Traverse the array
         for (int i = 0; i < nums.size(); ++i) {
